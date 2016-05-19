c8b6de3712b1ae3b35e2caaab19bbb28e5a2ab10
Ruben <rhartog@zilverline.com>
2016-05-19 10:50:42 +0200

    Use a 'real' unknown address, address was actually known

--- karma.conf.js
+++ karma.conf.js
@@ -81,7 +81,7 @@ module.exports = function(config) {
       '/split-chunked-response': 'http://localhost:2001/split-chunked-response',
       '/error-response': 'http://localhost:2001/error-response',
       '/echo-response': 'http://localhost:2001/echo-response',
-      '/unknown-host': 'http://unknown-host.com'
+      '/unknown-host': 'http://localhost:5678'
     },
 
 
